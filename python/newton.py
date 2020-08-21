d1049d80c8a69451b398d5c7f516149971b7bb02
paradisepx <brian@LAPTOP-8MASCD4F.localdomain>
2020-08-21 22:42:57 +0200

    correcting third root example

--- python/newton.py
+++ python/newton.py
@@ -9,9 +9,9 @@ def newton(function, derivative, x0, depth=4):
 
 # Find the third root of 27
 f = lambda x : 1/3*np.log(27)-np.log(x)
-Df = lambda x : -x
-x = newton(f, Df, 2, 50)
-print("The 3^3 is : {}".format(x))
+Df = lambda x : -1/x
+x = newton(f, Df, 2, 5)
+print("The third root of 27 is : {}".format(x))
 
 # Find the the golden ratio
 f = lambda x : x**2-x-1
