f80b37ecb6b940c86bc85d5f4a0cf2d80abb33af
paradisepx <brian@LAPTOP-8MASCD4F.localdomain>
2020-08-21 22:39:55 +0200

    added basic Newton's method function root estimator

---
+++ python/newton.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def newton(function, derivative, x0, depth=4):
+    x=x0
+    for i in range(depth) : x=x-function(x)/derivative(x)
+    return x
+
+
+
+# Find the third root of 27
+f = lambda x : 1/3*np.log(27)-np.log(x)
+Df = lambda x : -x
+x = newton(f, Df, 2, 50)
+print("The 3^3 is : {}".format(x))
+
+# Find the the golden ratio
+f = lambda x : x**2-x-1
+Df = lambda x : 2*x -1
+x = newton(f, Df, 4, 6)
+print("The golden ratio is : {}".format(x))
