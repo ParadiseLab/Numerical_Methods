64170a77b7baaaa2d5411bab52ab4cffccffd7e1
paradisepx <brian@LAPTOP-8MASCD4F.localdomain>
2020-08-22 14:16:37 +0200

    added riemann integral

---
+++ integration/riemann.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def riemann(function, p0, n : int):
+    x,y=p0
+    dx=np.abs(x-y)/n
+    r=0
+    for i in range(n):
+        r=r+function(x)*dx
+        x=x+dx
+    return r
+
+
+# find the value of pi by integrating sin(x)^2 from 0 to 2pi
+f = lambda x : np.sin(x)**2
+x = riemann(f, (0, 2*np.pi), int(1e6))
+print("The value of pi is : {}".format(x))
